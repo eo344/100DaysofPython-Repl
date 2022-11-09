828bcc22c879e56a2e85a410c62b6aa9934262a5
eo344 <115800060+eo344@users.noreply.github.com>
2022-11-09 16:08:06 +0000

    Create day3.py
    The Ultimate Wacky Recipe Maker
    You will need to:
    
    Create these as a variable:
    A type of food
    A type of plant
    A method of cooking
    A word to describe burned food
    A household item
    
    Output a nice looking Recipe page that *concatenates* a dish in this format:
    cooking food with burned plant on a bed of item

---
+++ day3/day3.py
@@ -0,0 +1,7 @@
+food = input("Name a food > ")
+plant = input("Name a type of plant > ")
+method = input("Name a method of cooking > ")
+burned = input("A word for burned food > ")
+diy = input("DIY Item > ")
+
+print("MENU \n", method, food, "with", burned, plant, "on a bed of", diy + "s")
