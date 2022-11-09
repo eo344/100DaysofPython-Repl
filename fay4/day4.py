162cbfcdb99f44df7babcf7d67bd57f537830029
eo344 <115800060+eo344@users.noreply.github.com>
2022-11-09 16:11:28 +0000

    Create day4.py
    Ask your users to list a bunch of information about them: things they like, things they hate, names of family and friends... it's up to you how many and what kinds of things you pick. Keep it wacky!
    
    Now construct your story - it can be about anything you want, but must use the variables you've created in step 1.
    
    Make sure to only work one paragraph at a time. Otherwise things could get a bit messy.

---
+++ fay4/day4.py
@@ -0,0 +1,15 @@
+name = input("Name: ")
+like = input("Like: ")
+superpower = input("Superpower: ")
+enemy = input("Enemy: ")
+weapon = input("Weapon: ")
+
+print("Our story begins wth our hero approaching a castle")
+print("Suddenly a bolt of lightning striked the ground at the feet of", name)
+print(" \033[31m'Our final battle begins!'\033[0m shouts the evil", enemy,
+      "clearly missing that fact that", name, "has the power of", superpower,
+      "which means they'll win quite easily")
+print(name, "swings their", weapon, "and uses", superpower,
+      "with it to strike", enemy, "and win immediately")
+print("'I think I'll go and enjoy some", like,
+      "now!' they say, our superhero can now go and celebrate their win")
