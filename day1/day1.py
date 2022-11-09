35aa068584e899fa8a63ca48d14cda9e1fcd1dbb
eo344 <115800060+eo344@users.noreply.github.com>
2022-11-09 16:04:35 +0000

    Create day1.py

---
+++ day1/day1.py
@@ -0,0 +1,23 @@
+print("Hello Replit")
+print ("and how are you?")
+
+print("""
+    Here's an exact
+      BIT
+        OF
+          TEXT
+""")
+
+print("What could go wrong?")
+
+print("---------------------------------------------------------")
+
+print("Emerald")
+print("June 12 2003")
+print("""
+I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time evry dday coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happen tobe. No excuses for not coding from the middle of a field!
+I am feeling 🥳
+You cannot follow my progress lo, leave my alone <3
+""")
